1906b3126416ba9fe9a5398787e1a12f383a7946
lucie brismontier <luciebtdev@gmail.com>
2020-01-28 21:08:16 +0100

    pseudo code controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :moments
+  get "/carrousel", to: "moments#carrousel"
+  resources :friends
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
