06a0986745a8534d0b05ba602a5b6c527092ecb1
lucie brismontier <luciebtdev@gmail.com>
2020-01-30 18:49:59 +0100

    migration timer user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_201151) do
+ActiveRecord::Schema.define(version: 2020_01_30_174633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_01_28_201151) do
     t.string "first_name"
     t.string "last_name"
     t.string "avatar"
+    t.integer "time_left_today"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
