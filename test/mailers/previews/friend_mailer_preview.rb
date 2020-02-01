920271c7601d5f7b6a3a5da05d05fb7165cec782
Lukas Metzmacher <lukas.metzmacher@bild.de>
2020-02-01 16:56:18 +0100

    adding friend maler

---
+++ test/mailers/previews/friend_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/friend_mailer
+class FriendMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/friend_mailer/welcome
+  def welcome
+    FriendMailer.welcome
+  end
+
+end
