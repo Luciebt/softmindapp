f2574d355f5016f447afe4a0b270e8e43e09924a
lucie brismontier <luciebtdev@gmail.com>
2020-02-03 13:13:59 +0100

    adding play method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+  end
+
+  def play
     @moments = Moment.where(seen: false).sample(4)
   end
+
 end
