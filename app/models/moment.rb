10b6bdac7234bf5e98f05d8ce2f1f01c24135bf0
lucie brismontier <luciebtdev@gmail.com>
2020-02-11 19:25:26 +0100

    private method after commit

--- app/models/moment.rb
+++ app/models/moment.rb
@@ -3,6 +3,7 @@ class Moment < ApplicationRecord
 
   mount_uploader :media, PhotoUploader
   belongs_to :user
+  after_create_commit :assign_type
 
   VIDEO_TYPES = %w(mp4 mov avi wmv mkv flv 3gp).freeze
   IMG_TYPES = %w(jpg png jpeg gif bmp tiff pdf).freeze
@@ -11,4 +12,13 @@ class Moment < ApplicationRecord
   # create enum with all media types
   # add media type field in moments table
   # regex to file extension
+
+
+  private
+
+  def assign_type
+    puts self.media.resource_type
+    # self.update(media_type: self.media.resource_type)
+  end
+
 end
