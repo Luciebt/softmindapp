9ea7544ac57e72af18c015bd66bf04ecefe6465e
Rokas Lukosius <tarakonas1@gmx.de>
2020-02-15 10:47:34 +0100

    added some js for fiends view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import "bootstrap";
 
 import { log, setup, loadVideo } from '../play';
 import { carrouselFct } from '../carrousel';
+import { friendsEditToggle } from '../friends';
 
 setup();
 carrouselFct();
+friendsEditToggle();
