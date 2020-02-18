353745c6b541380d700e72d7ff948e53d4a71ca4
lucie brismontier <luciebtdev@gmail.com>
2020-02-18 20:30:09 +0100

    fix everything

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://www.softmind.app" }
+  config.action_mailer.default_url_options = { host: "softmind.app" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
